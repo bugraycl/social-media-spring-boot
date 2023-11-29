748433e18e177154d020e2ddc07d931d9b216965
Buğra YÜCEL <byuucelx@gmail.com>
2023-11-29 05:35:47 +0300

    create: user controller

--- src/main/java/com/bugraycl/socialapp/controller/UserController.java
+++ src/main/java/com/bugraycl/socialapp/controller/UserController.java
@@ -4,15 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
-
-    @GetMapping
-    public String homeControllerHandler() {
-        return "this is home controller";
+public class UserController {
+    @GetMapping("/users")
+    public String getUsers() {
+        return "get all users";
     }
-
-    //@DeleteMapping
-    //@PostMapping
-
-
 }
